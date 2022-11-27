7daf45d98dbb48fddb81ad72315736a6116bd9e0
Mmunni <masumamunni20@gmail.com>
2022-11-27 12:19:51 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-import logo from "./logo.svg";
-import "./App.css";
+import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import router from './Router/Router/Router';
 
 function App() {
   return (
-    <div>
-      <div className="card w-96 bg-base-100 shadow-xl">
-        <figure>
-          <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
-        </div>
-      </div>
+    <div className='max-w-[1140px] mx-auto'>
+     <RouterProvider router={router}></RouterProvider>
+     <Toaster/>
     </div>
   );
 }
---
+++ src/Router/Router/Router.js
@@ -0,0 +1,33 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../layouts/Main";
+import Home from "../../Pages/Home/Home/Home";
+import Login from "../../Pages/Login/Login";
+import SignUp from "../../Pages/SignUp/SignUp";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main/>,
+        Children:[
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/signup',
+                element: <SignUp></SignUp>
+            },
+        ]
+    },
+    {
+        path: '*',
+        element: <h3 className="text-center">Page not Found</h3>
+    }
+   
+])
+
+export default router;
